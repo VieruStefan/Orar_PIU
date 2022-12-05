be085c5dac3f9ec48bc0a52cf669d146d367f88b
Vieru Stefan <stefan.vieru@student.tuiasi.ro>
2022-12-05 19:42:46 +0200

    #

--- Orar/main.py
+++ Orar/main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QMessageBox, QApplication, QVBoxLayout, QWidget, QTabWidget
-from MainWindow import MainWindow
+from interface.MainWindow import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
